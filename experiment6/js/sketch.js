42fc891fe8f741d0289e3fa2cbb22e35d6345a06
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2025-02-17 18:14:12 -0800

    completed haiku generator

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -106,6 +106,35 @@ var seven = {
 };
 
 var fiveEnd = {
+  "start": "#fiveEnd#",
+  "fiveEnd": ["#212#", "#311#", "#221#", "#1211#"],
+  
+
+  "212": ["#2adj# #1noun# #2verb#", "#2noun# #1verb# #2adj#", "#2verb# #1adverb# #2noun#", "#2adverb# #1verb# #2noun#", "#2noun# #1conjunction# #2noun#"],
+  "311": ["#3participle# #1noun# #1verb#", "#3verb# #1noun# #1verb#", "#3noun# #1verb# #1noun#", "#3participle# #1verb# #1noun#"],
+  "221": ["#2noun# #2verb# #1noun#", "#2verb# #2noun# #1verb#", "#2adj# #2noun# #1verb#", "#2adj# #2verb# #1noun#"],
+  "1211": ["#1noun# #2attrnoun# #1noun# #1verb#", "#1adj# #2noun# #1noun# #1verb#", "#1noun# #2verb# #1noun# #1verb#", "#1adj# #2verb# #1noun# #1verb#"],
+
+  "1noun": ["sky", "rain", "wind", "moon", "sun", "star", "tree", "bird", "leaf", "snow", "wave", "rock", "cloud", "mist", "grass", "stone", "sea", "fire", "path", "frost", "thorn", "dawn", "ash", "glimpse", "flock", "breeze", "chill", "cave", "haze", "veil", "gale", "grove", "cliff", "flame", "mud", "wisp", "dusk", "crest", "shard", "stream", "flame"],
+  "2noun": ["mountain", "ocean", "forest", "morning", "sunrise", "shadow", "garden", "river", "petal", "meadow", "island", "echoes", "thunder", "raindrop", "valley"],
+  "3noun": ["butterfly", "cicada", "memory", "mountain top", "festival", "evening sky", "paradise", "horizon", "riverbed", "sunshine", "shadowed path", "universe"],
+
+  "1adj": ["cold", "dark", "soft", "bright", "clear", "still", "wild", "sharp", "calm", "green", "wet", "thin", "deep", "harsh", "sweet", "rough", "fresh", "tall", "smooth", "soft"],
+  "2adj": ["quiet", "gentle", "simple", "honest", "empty", "noble", "timid", "sleepy", "crimson", "frozen", "lively", "bitter", "sacred", "cheerful", "fragile", "sandy"],
+  "3adj": ["beautiful", "mysterious", "delicate", "harmonious", "sensitive", "radiant", "elegant"],
+
+  "1verb": ["runs", "falls", "sings", "jumps", "soars", "flies", "push", "pull", "cuts", "climbs", "dives", "glides", "skips", "snaps", "swism", "bends", "shines", "blinks", "melts", "splash", "bloom", "chase"],
+  "2verb": ["whisper", "flutter", "wander", "shiver", "glisten", "travel", "stumble", "glimmer", "murmur", "gather", "follow", "hasten", "tumble", "sizzle"],
+  "3verb": ["whispering", "gathering", "fluttering", "vanishing", "shimmering", "transforming", "compelling", "questioning", "rejoicing", "reflecting", "becoming", "arriving", "surviving", "expanding", "emerging", "nurturing", "returning", "belonging", "releasing", "creating", "ascending", "uniting"],
+
+  "1adverb": ["fast", "slow", "high", "low", "soft", "still", "far", "near", "then", "now", "down", "up", "in", "out", "wide", "deep", "long"],
+  "2adverb": ["gently", "slowly", "softly", "early", "quickly", "brightly", "calmly", "loudly", "highly", "barely", "safely", "sweetly", "roughly", "warmly"],
+
+  "1conjunction": ["and", "but", "or", "nor"],
+
+  "3participle": ["shuddering", "fluttering", "shivering", "ascending", "expanding", "glistening",  "reflecting", "vanishing", "receding", "blossoming", "tremoring"],
+
+  "2attrnoun": ["sunlight", "moonlight", "starfish", "seabreeze", "rainfall", "snowfall", "riverbed", "windmill", "firefly", "grassland", "pinecone", "birdsong", "earthquake", "rockfall", "woodland", "summer", "winter", "spring"]
 
 }
 
@@ -133,8 +162,12 @@ function setup() {
   grammarLine2 = tracery.createGrammar(seven);
   var resultLine2 = grammarLine2.flatten("#start#");
 
+  grammarLine3 = tracery.createGrammar(fiveEnd);
+  var resultLine3 = grammarLine3.flatten("#start#");
+
   console.log(resultLine1);
   console.log(resultLine2);
+  console.log(resultLine3);
 
 
   
