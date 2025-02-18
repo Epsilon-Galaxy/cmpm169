8c12182912473fe1623458ebfe660dbbae03add4
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2025-02-17 16:13:42 -0800

    Finished the second line of a haikui

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -72,6 +72,36 @@ var fiveStart = {
 };
 
 var seven = {
+  "start": "#sevenStart#",
+  "sevenStart": ["#211111#", "#22111#", "#1111112#", "#322#"],
+
+  "2111111": ["#2adj# #1noun# #1verb# #1verb# #1verb# #1verb#", "#2noun# #1verb# #1adverb# #1verb# #1verb# #1verb#", "#2noun# #1verb#, #1adj# #1noun# #1adverb# #1verb#", "#2adj# #1noun#, #1noun# #1verb# #1adverb# #1verb#"],
+  "22111": ["#2adj# #2noun#, #1noun# #1adverb# #1verb#", "#2noun# #2verb#, #1adj# #1noun# #1verb#", "#2prep# #2noun#, #1noun# #1verb# #1adverb#", "#2adj# #2noun#, #1verb# #1prep# #1noun#"],
+  "1111112": ["#1noun# #1verb# #1noun# #1verb# #1adj# #2noun#", "#1noun# #1verb# #1noun# #1verb# #1adj# #1noun# #2verb#"],
+  "322": ["#3adj# #2noun# #2verb#", "#3noun# #2verb# #2adverb#", "#3noun# #2noun# #2verb#", "#3verb# #2prep# #2noun#", "#3noun# #2adj# #2noun#", "#3adj# #2noun# #2adverb#"],
+
+  "1noun": ["sky", "rain", "wind", "moon", "sun", "star", "tree", "bird", "leaf", "snow", "wave", "rock", "cloud", "mist", "grass", "stone", "sea", "fire", "path", "frost", "thorn", "dawn", "ash", "glimpse", "flock", "breeze", "chill", "cave", "haze", "veil", "gale", "grove", "cliff", "flame", "mud", "wisp", "dusk", "crest", "shard", "stream", "flame"],
+  "2noun": ["mountain", "ocean", "forest", "morning", "sunrise", "shadow", "garden", "river", "petal", "meadow", "island", "echoes", "thunder", "raindrop", "valley"],
+  "3noun": ["butterfly", "cicada", "memory", "mountain top", "festival", "evening sky", "paradise", "horizon", "riverbed", "sunshine", "shadowed path", "universe"],
+
+  "1verb": ["runs", "falls", "sings", "jumps", "soars", "flies", "push", "pull", "cuts", "climbs", "dives", "glides", "skips", "snaps", "swism", "bends", "shines", "blinks", "melts", "splash", "bloom", "chase"],
+  "2verb": ["whisper", "flutter", "wander", "shiver", "glisten", "travel", "stumble", "glimmer", "murmur", "gather", "follow", "hasten", "tumble", "sizzle"],
+  "3verb": ["whispering", "gathering", "fluttering", "vanishing", "shimmering", "transforming", "compelling", "questioning", "rejoicing", "reflecting", "becoming", "arriving", "surviving", "expanding", "emerging", "nurturing", "returning", "belonging", "releasing", "creating", "ascending", "uniting"],
+
+  "1adj": ["cold", "dark", "soft", "bright", "clear", "still", "wild", "sharp", "calm", "green", "wet", "thin", "deep", "harsh", "sweet", "rough", "fresh", "tall", "smooth", "soft"],
+  "2adj": ["quiet", "gentle", "simple", "honest", "empty", "noble", "timid", "sleepy", "crimson", "frozen", "lively", "bitter", "sacred", "cheerful", "fragile", "sandy"],
+  "3adj": ["beautiful", "mysterious", "delicate", "harmonious", "sensitive", "radiant", "elegant"],
+
+
+  "1adverb": ["fast", "slow", "high", "low", "soft", "still", "far", "near", "then", "now", "down", "up", "in", "out", "wide", "deep", "long"],
+  "2adverb": ["gently", "slowly", "softly", "early", "quickly", "brightly", "calmly", "loudly", "highly", "barely", "safely", "sweetly", "roughly", "warmly"],
+
+
+  "1prep": ["in", "on", "by", "to", "at", "from", "with", "through"],
+  "2prep": ["above", "below", "within", "without", "between", "beneath", "beyond", "inside", "upon", "around"],
+
+
+
 
 };
 
@@ -79,7 +109,9 @@ var fiveEnd = {
 
 }
 
-var grammar;
+var grammarLine1;
+var grammarLine2;
+var grammarLine3;
 
 
 // setup() function is called once when the program starts
@@ -94,10 +126,14 @@ function setup() {
   });
   resizeScreen();
 
-  grammar = tracery.createGrammar(fiveStart);
+  grammarLine1 = tracery.createGrammar(fiveStart);
+  var resultLine1 = grammar.flatten("#start#");
+  
+  grammarLine2 = tracery.createGrammar(seven);
+  var resultLine2 = grammar.flatten("#start#");
 
-  var result = grammar.flatten("#start#");
-  console.log(result);
+  console.log(resultLine1);
+  console.log(resultLine2);
 
 
   
